6be01f0daf523946c2712ccac67ef6dbe860d52e
skuzminich <8dJkzdHGpxxn>
2020-04-09 11:43:27 +0300

    Sub models implemented. Plus passedParameters contains only URL, not body parameters

--- src/controller.php
+++ src/controller.php
@@ -76,12 +76,7 @@ class controller extends abstractJsonApiController {
 
         $url = $_SERVER['REQUEST_URI'];
 
-        $bodyParameters = $this->bodyParameters;
-        if ($this->verb === 'POST') {
-            $bodyParameters = $this->passedParameters;
-        }
-
-        $security->validateSignature($this->signature, $this->verb, $url,  $bodyParameters, $security->getSecurityClient(), $security->getSecuritySession());
+        $security->validateSignature($this->signature, $this->verb, $url, $this->bodyParameters, $security->getSecurityClient(), $security->getSecuritySession());
     }
 
     /**
@@ -92,18 +87,13 @@ class controller extends abstractJsonApiController {
 
         if (!(isset($uri) && $uri === '/')) {
             $variables = array_filter(explode('/', substr($uri, 1)), 'strlen');
-
-            $isModelVariable = true;
-            foreach ($variables as $variable) {
-                if ($isModelVariable && stripos($variable, 'v') === 0 && is_numeric(substr($variable, 1, 1)) && strpos($variable, '.') !== 0){
-                    $this->version = $variable;
-                } else if ($isModelVariable && !is_numeric($variable)) {
-                    $this->modelName = str_replace('-', '\\', $variable);
-                    $isModelVariable = false;
-                } else {
-                    $this->passedParameters[] = $variable;
-                }
+            $variable = current($variables);
+            if (stripos($variable, 'v') === 0 && is_numeric(substr($variable, 1, 1)) && strpos($variable, '.') !== 0){
+                $this->version = $variable;
+                array_shift($variables);
             }
+
+            $this->passedParameters = $this->parseModelNameFromUri($variables);
         }
     }
 
