1f7eb7256eb7a24c87b409491e2f08aa0f2a3809
skuzminich <8dJkzdHGpxxn>
2020-04-21 15:24:51 +0300

    Parameter decryption implemented after minimalism update

--- src/abstracts/abstractModel.php
+++ src/abstracts/abstractModel.php
@@ -5,11 +5,13 @@ use carlonicora\minimalism\core\modules\abstracts\models\abstractApiModel;
 use carlonicora\minimalism\core\services\exceptions\configurationException;
 use carlonicora\minimalism\core\services\exceptions\serviceNotFoundException;
 use carlonicora\minimalism\core\services\factories\servicesFactory;
+use carlonicora\minimalism\services\encrypter\encrypter;
 use carlonicora\minimalism\services\jsonapi\interfaces\responseInterface;
 use carlonicora\minimalism\services\jsonapi\responses\dataResponse;
 use carlonicora\minimalism\services\jsonapi\responses\errorResponse;
 use carlonicora\minimalism\services\MySQL\exceptions\dbRecordNotFoundException;
 use carlonicora\minimalism\services\MySQL\exceptions\dbSqlException;
+use Exception;
 
 abstract class abstractModel extends abstractApiModel {
     /** @var dataResponse  */
@@ -18,6 +20,9 @@ abstract class abstractModel extends abstractApiModel {
     /** @var errorResponse|null  */
     protected ?errorResponse $error=null;
 
+    /** @var encrypter */
+    protected $encrypter;
+
     /**
      * model constructor.
      * @param servicesFactory $services
@@ -25,10 +30,22 @@ abstract class abstractModel extends abstractApiModel {
      * @param string $verb
      * @param array $file
      * @throws serviceNotFoundException
+     * @throws Exception
      */
-    public function __construct(servicesFactory $services, array $passedParameters, string $verb, array $file=null){
+    public function __construct(servicesFactory $services, array $passedParameters, string $verb, array $file=null) {
+        // Encrypter should be initialises before calling parent construct
+        $this->encrypter = $services->service(encrypter::class);
         parent::__construct($services, $passedParameters, $verb, $file);
         $this->response = new dataResponse();
+    }
+
+    /**
+     * @param string $parameter
+     * @return string
+     */
+    protected function decryptParameter(string $parameter) : string {
+        // Override this method if you need decryption
+        return $this->encrypter->decryptId($parameter);
     }
 
     /**
